63d3c5a666397f5f8635bbd1cefcf3e4de76d3fa
田赐 <1037872820@qq.com>
2020-08-27 19:47:13 +0800

    项目登录页面完成

---
+++ src/components/HmLog.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="hm-log">
+    <span class="iconfont iconnew"></span>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,16 @@ import './styles/iconfont.css'
 // 导入vant
 import Vant from 'vant'
 import 'vant/lib/index.css'
+// 引入'amfe-flexible'安装包
+import 'amfe-flexible'
+// 全局引入hmheader组件
+import HmHeader from './components/HmHeader.vue'
+// 全局引入log
+import HmLog from './components/HmLog.vue'
+
+Vue.component('hm-header', HmHeader)
+
+Vue.component('hm-log', HmLog)
 
 // 全局的把vant所有的组件都导入好了
 Vue.use(Vant)
